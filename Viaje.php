c66909987b46b214eae66afce00254886878db55
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2024-04-06 22:01:22 -0300

    Adicion de metodos propio en clase Viaje

--- Viaje.php
+++ Viaje.php
@@ -1,4 +1,6 @@
 <?php
+include_once 'Pasajero.php';
+include_once 'ResponsableViaje.php';
 
 class Viaje{
 
@@ -13,6 +15,7 @@ class Viaje{
         $this->codigo=$unCodigo;
         $this->cantidadMaximaPasajeros=$unaCantidadMaxima;
         $this->destino=$unDestino;
+        $this->objPasajero=array();
     }
 
     //Observadores
@@ -47,5 +50,78 @@ class Viaje{
      */
     public function getPasajeros(){
         return $this->objPasajero;
+    }
+
+    //Modificadores
+
+    /**
+     * Modifica el valor del atributo codigo de la instancia por el valor pasado por parametro
+     * @param int $unCodigo
+     */
+    public function setCodigo($unCodigo){
+        $this->codigo=$unCodigo;
+    }
+
+    /**
+     * Modifica el valor del atributo cantidadMaximaPasajeros de la instancia por el valor pasado por parametro
+     * @param int $unaCantidad
+     */
+    public function setCantidadMaximaPasajeros($unaCantidad){
+        $this->cantidadMaximaPasajeros=$unaCantidad;
+    }
+
+    /**
+     * Modifica el valor del atributo destino de la instancia por el valor pasado por parametro
+     * @param string $unDestino
+     */
+    public function setDestino($unDestino){
+        $this->destino=$unDestino;
+    }
+
+    /**
+     * Modifica el valor del atributo objPasajero de la instancia por el valor pasado por parametro
+     * @param int $unArreglo
+     */
+    public function setPasajeros($unArreglo){
+        $this->objPasajero=$unArreglo;
+    }
+
+    //Propios
+
+    /**
+     * Retorna la cantidad de pasajeros en el arreglo de Pasajero
+     * @return int
+     */
+    public function cantidadPasajeros(){
+        return count($this->getPasajeros());
+    }
+
+    /**
+     * Retorna el pasajero en la posicion indicada por parametro dentro del arreglo de Pasajero
+     * @param int $posicion
+     * @return Pasajero
+     */
+    public function darPasajeroEn($posicion){
+        return $this->getPasajeros()[$posicion];
+    }
+
+    /**
+     * Verifica si el pasajero ingresado por parametro se encuentra en el arreglo de Pasajero
+     * Compara los numeros de documento para determinar si son iguales o no
+     * @param Pasajero $unPasajero
+     * @return boolean
+     */
+    public function estaEnLista($unPasajero){
+        $encontrado=false;
+        if($this->cantidadPasajeros()!=0){
+            $i=0;
+            while($i<$this->cantidadPasajeros() && !$encontrado){
+                if($unPasajero->getNumeroDocumento()==$this->darPasajeroEn($i)->getNumeroDocumento()){
+                    $encontrado=true;
+                }
+                $i++;
+            }
+        }
+        return $encontrado;
     }
 }
